ce41867b4c77e75ce377034f1edda793580fd9e2
Alex Lementuev <a.lementuev@gmail.com>
2012-02-21 16:08:17 +0300

    Add Localization.h

---
+++ classes/Localization.h
@@ -0,0 +1,9 @@
+//
+//  Localization.h
+//  champions
+//
+//  Created by Alex Lementuev on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#define LocalizedString(key, comment) comment
---
+++ classes/Localization.m
@@ -0,0 +1,13 @@
+//
+//  Localization.m
+//  champions
+//
+//  Created by Alex Lementuev on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Localization.h"
+
+@implementation Localization
+
+@end
